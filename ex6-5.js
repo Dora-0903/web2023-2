02c393407bfe21d6a78df6b241db63f19ee10a9e
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2023-10-17 10:19:45 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -3,3 +3,4 @@ function sayhello65() {alert('Hello,' + who65 + '!');}
 function taro65() {who65 = '太郎';}
 function hanako65() {who65 = '花子';}
 function taroinput() {alert('太郎が来た');}
+function hanakoinput() {alert('花子が来た');}
