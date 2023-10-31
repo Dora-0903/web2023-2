5fa6f9c152d9bc697ac4ad0e6b1bbdd3b7be72a5
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2023-10-31 09:50:52 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function gazo688() {document.getElementByID ('who688').innerHTML = '<img src="./image/denkyuu_on.jpg" alt="オン">';}
+function gazo688() {document.getElementByID ('who688').innerHTML = '<img src="./image/denkyuu_on.jpg" alt="電気オン">';}
