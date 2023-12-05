81e4aa4184c3a87070bfb7060e43a40996f1cb4c
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2023-12-05 10:37:39 +0900

    Update ex07-7.js

--- ex07-7.js
+++ ex07-7.js
@@ -1,4 +1,4 @@
-unction nazo77() {if (document.getElementById('answer77').value = '夢')
+function nazo77() {if (document.getElementById('answer77').value = '夢')
                   {alert('あたり！');}
                   else{alert('はずれ！');}
                   }
