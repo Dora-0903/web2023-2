0a02a0c337a9ff55ff74e05fc7208562ce91a1d9
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2023-12-05 10:30:13 +0900

    Create ex07-6.js

---
+++ ex07-6.js
@@ -0,0 +1,4 @@
+function nazo76() {if (document.getElementById('answer76').value !== '夢') // !==で値か型が等しくないことを指す
+                  {alert('はずれ！');}
+                  else{alert('あたり！');}
+                  }
