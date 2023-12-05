dc8c4b6765a1323c0524e9bace56e10287c11087
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2023-12-05 09:56:57 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,3 +1,3 @@
 function BMI() {var x = Number(document.getElementById('input3').value) / 100;
                 var y = Number(document.getElementById('input4').value);
-                alert(y/(x*x))}
+                alert('あなたのBMI値は' + y/(x*x) + 'です。');}
