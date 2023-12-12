b330f0dcccd5ddd34d072a1ab057bbf8789ab7c8
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2023-12-12 09:59:45 +0900

    Create ex07-10.js

---
+++ ex07-10.js
@@ -0,0 +1,3 @@
+function BMI2() {var x = Number(document.getElementById('height1').value) / 100; //ここで身長をメートルに変換している。
+                var y = Number(document.getElementById('body weight1').value);
+                 
