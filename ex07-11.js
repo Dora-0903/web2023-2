fe963346daffe571962b990e797350db33c4cd55
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2023-12-18 00:05:17 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -1,4 +1,4 @@
-function determineLeapYear() {var year = Number(document.getElementById('year').value);
+function leapyearjudge() {var year = Number(document.getElementById('year').value);
 //西暦を変数「year」に代入　↑
 
                      var yearjudge; //変数を設定
