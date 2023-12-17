5ff702b1957fbb242460166bed73a2828955a9f0
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2023-12-17 21:36:20 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,5 +1,5 @@
 function BMI2() {var height1 = Number(document.getElementById('height1').value) / 100; //ここで身長をメートルに変換している。
-                var bodyweight1 = Number(document.getElementById('body weight1').value);
+                var body weight1 = Number(document.getElementById('body weight1').value);
                  var bmi1 = body weight1 / (height1 * height1); //ここでBMI値の計算
 
                  var bmialert; //ここで空の変数を用意しておく。なぜなら、まとめて表示する際に必要だから。
