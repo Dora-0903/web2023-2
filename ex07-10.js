ce08e99727c2578cab2c5354be560ba07094b434
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2023-12-18 00:06:55 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -4,12 +4,12 @@ function BMI2() {var height1 = Number(document.getElementById('height1').value) 
 
                  var bmialert; //ここで空の変数を用意しておく。なぜなら、まとめて表示する際に必要だから。
                  
-                 if (bmi1 < 18.5) {bmialert = '低体重'} //変数「bmialert」に「低体重」を入れている。
+                 if (bmi1 < 18.5) {bmialert = '低体重';} //変数「bmialert」に「低体重」を入れている。
                  else if
-                   (bmi1 < 25) {bmialert = '普通'} //変数「bmialert」に「普通」を入れている。
+                   (bmi1 < 25) {bmialert = '普通';} //変数「bmialert」に「普通」を入れている。
                  else
-                 {bmialert = '肥満'}
+                 {bmialert = '肥満';}
 
-                 alert('あなたのBMI値は' + bmi1 + 'です。')
-                 alert('あなたの肥満度は' + bmialert + 'です。')
+                 alert('あなたのBMI値は' + bmi1 + 'です。');
+                 alert('あなたの肥満度は' + bmialert + 'です。');
                 }
